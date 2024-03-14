d9c057a6b03154572f4d35032016a82dc50089ff
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-14 16:17:36 +0100

    added card title

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,5 +3,14 @@
 @section('title', 'Create Comic')
 
 @section('main-content')
-    <h1>Create Comic Page</h1>
+<section id="add-comic" class="container">
+
+<div class="card-title">
+    <h1>Add new Comic</h1>
+    <div class="button">
+        <a class="button" href="{{ route('comics.index') }}">Back to Comics</a>
+    </div>
+</div>
+
+</section>
 @endsection
