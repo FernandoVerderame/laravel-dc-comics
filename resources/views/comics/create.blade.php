a27810138d896c5bf097ab5d75e2e1af5f676cb5
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-14 16:43:24 +0100

    added description input

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -14,10 +14,17 @@
 
 <div class="card-body">
     <form action="" method="">
+
         <div class="input-section">
             <label for="title">Title</label>
             <input type="text" name="title" id="title" placeholder="Ex.: Suicide Squad Vol.3 Rouges">
         </div>
+
+        <div class="input-section">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="3"></textarea>
+        </div>
+
     </form>
 </div>
 
